5115320c208400369c605c5ffd894dc7ce2c58cb
Arthur van Aarssen <arthur.vanaarssen@ns.nl>
2024-04-24 14:28:25 +0200

    build: extended test to check for a chessboard

--- app/src/androidTest/java/com/ns/iskingincheckandroid/HomePageTest.kt
+++ app/src/androidTest/java/com/ns/iskingincheckandroid/HomePageTest.kt
@@ -1,6 +1,9 @@
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.ns.iskingincheckandroid.MainActivity
 import org.junit.Rule
@@ -14,10 +17,20 @@ class HomePageTest {
     val composeTestRule = createAndroidComposeRule<MainActivity>()
 
     @Test
-    fun checkButtonIsDisplayed() {
-        // Test the actual button from MainActivity without setting new content
+    fun checkButtonIsDisplayedAndClickable() {
+        // Click the button
         composeTestRule
             .onNodeWithText("Start the game, hombre!")
+            .assertIsDisplayed()
+            .performClick()
+
+        // Fetch all nodes tagged as "ChessCell" and verify there are exactly 64 such nodes
+        val chessCells = composeTestRule.onAllNodes(hasTestTag("ChessCell"))
+        assert(chessCells.fetchSemanticsNodes().size == 64) // Make sure there are exactly 64 cells
+
+        // If required, check the visibility of the first cell (assuming all are similarly managed)
+        composeTestRule
+            .onNode(hasTestTag("ChessCell"), useUnmergedTree = true) // Adjust tag if each cell has a unique tag
             .assertIsDisplayed()
     }
 }
